80904cda7c5cb39c7741a5366e37da953ac1c072
Senku24 <nixonpaule24@gmail.com>
2026-07-08 23:03:34 +0530

    deleteEndpointAdded

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ const users=[{
     username: 'john',
     password: '1234'
 }];
-const expense=[{
+const expenses=[{
+    userId: 1,
     id: 1,
     title: 'chicken',
     category: 'food',
@@ -50,12 +51,39 @@ app.post('/signin', (req, res) => {
     res.status(200).json({ message: 'User signed in successfully', token });
 });
 
-app.post('/expense', middleware, (req, res) => {})
+app.post('/expense', middleware, (req, res) => {
+    const { title, category, amount, date } = req.body;
+    const userId = req.userId;
+    if (!title || !category || amount == null || !date || !userId) {
+        return res.status(400).json({
+        message: "All fields are required (title, category, amount, date, userId)."
+    });
+}
+    const newExpense = {
+        userId: userId,
+        id: expenseId++,
+        title,
+        category,
+        amount: Number(amount),
+        date
+    };
+
+    expenses.push(newExpense);
+    res.status(201).json({ message: 'Expense created successfully', expense: newExpense });
+})
 
 
 // read endpoints: 
-app.get('/', middleware, (req, res) => {})
+app.get('/', middleware, (req, res) => {
+    res.send('/Users/nixonpaul/Code_learn/WebDev26/expense_tracker/Expense_tacker/frontend/dashboard.html');
 
+})
+app.get('/signup', (req, res) => {
+    res.send('/Users/nixonpaul/Code_learn/WebDev26/expense_tracker/Expense_tacker/frontend/signup.html');
+});
+app.get('/signin', (req, res) => {
+    res.send('/Users/nixonpaul/Code_learn/WebDev26/expense_tracker/Expense_tacker/frontend/signin.html');
+});
 app.get('/expense', middleware, (req, res) => {})
 app.get('/expense/:id', middleware, (req, res) => {})
 
@@ -66,7 +94,18 @@ app.get('/expense/summary', middleware, (req, res) => {})
 app.put('/expense/:id', middleware, (req, res) => {})
 
 //delete endpoints:
-app.delete('/expense/:id', middleware, (req, res) => {})
+app.delete('/expense/:id', middleware, (req, res) => {
+    const expenseId = parseInt(req.params.id);
+    const userId = req.userId;
+
+    const expenseIndex = expenses.findIndex(expense => expense.id === expenseId && expense.userId === userId);
+    if(expenseIndex === -1){
+        return res.status(401).json({message: 'expense not found or you are not authorized to delete this expense'});
+    }
+
+    expenses.splice(expenseIndex, 1);
+    res.status(200).json({message: 'expense deleted successfully'});
+})
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
